f0e94741ba3ce4111e3d62116d0066064438de1f
rhidayat122 <151610243@smkassalaambandung.sch.id>
2017-07-11 15:27:02 +0700

    6.3.5

---
+++ app/BorrowLog.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Collection;
+
+
+class BorrowLog extends Model
+{
+    protected $fillable=['book_id','user_id','is_returned'];
+
+    public function book()
+    {
+    	return $this->belongsTo('App\Book');
+    }
+    public function user()
+    {
+    	return $this->belongsTo('App\User');
+    }
+
+    protected $casts = ['is_returned' => 'boolean',];
+
+    public function scopeReturned($query)
+    {
+    	return $query->where('is_returned', 1);
+    }
+
+    public function scopeBorrowed($query)
+    {
+    	return $query->where('is_returned', 0);
+    }
+}
